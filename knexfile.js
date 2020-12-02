4ae93e012f901904fd842246b23a1270c6586735
Samuel Nogueira <samuelnbacelar@hotmail.com>
2020-12-02 10:26:52 -0300

    agora vai

--- knexfile.js
+++ knexfile.js
@@ -75,12 +75,7 @@ module.exports = {
 
   production: {
     client: process.env.DB_CLIENT,
-    connection: {
-      host : process.env.DB_HOST,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASSWORD,
-      database : process.env.DB_DATABASE
-    }, 
+    connection: process.env.DATABASE_URL, 
     migrations : {
       tableName :'knex_migrations',
       directory : `${__dirname}/src/database/migrations`
